2533a0bb062cd1cd586e121c0ac9c7c3ef303775
fakhryhizballah <falehry88@gmail.com>
2025-01-06 09:17:41 +0700

    map interaktif

--- public/js/map2.js
+++ public/js/map2.js
@@ -1,5 +1,12 @@
 // Inisialisasi Peta
+
 const map = L.map('map').setView([-0.04169758550253827, 109.3362421903293], 13);
+// Mendapatkan Lokasi Sekarang
+navigator.geolocation.getCurrentPosition(function (position) {
+    const lat = position.coords.latitude;
+    const lng = position.coords.longitude;
+    map.setView([lat, lng], 18);
+});
 
 // Tambahkan Tile Layer
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
@@ -24,8 +31,27 @@ function addMarkers(maker) {
     for (let a of maker) {
         let lat = a.Location.lat;
         let lng = a.Location.lng;
+        console.log(a);
+        let random = Math.floor(Math.random() * 1000000);
+        let id64 = btoa(a.id);
+        let key = btoa(random + "#" + id64 + "#" + btoa(Math.floor(Math.random() * 1000)));
+        console.log(key);
+        // window.location.href = "/admin/umkm/entity/" + key;
+        const routeLink = `https://www.google.com/maps/dir/?api=1&destination=${lat},${lng}`;
+        const profileLink = `/admin/umkm/entity/${key}`;
 
-        let marker = L.marker([lat, lng]).bindPopup(`Lat: ${lat}, Lng: ${lng}`);
+        const popupContent = `
+    <h3>${a.badanusaha}</h3><br>
+    Kategori: ${a.kategoriusaha}<br><br>
+<a href="${routeLink}" class="btn btn-info mx-1" target="_blank" style="padding: 8px 12px;">
+    Lihat Lokasi
+</a>
+<a href="${profileLink}"class="btn btn-info mx-1" target="_blank" style="padding: 8px 12px;">
+    Lihat Profile
+</a>
+`;
+
+        let marker = L.marker([lat, lng]).bindPopup(popupContent);
         markers.addLayer(marker);
     }
 
